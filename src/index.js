330e2cb88e478ec43e35b5a9a159d571cd82411a
Eduardo J. Guerra <edujguerra@gmail.com>
2023-01-19 23:10:59 -0300

    2301192310

--- src/index.js
+++ src/index.js
@@ -1,17 +1,78 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function MainHeader() {
+  return <h1 className="text-primary">REACT COURSE</h1>
+}
+
+const subHeaderStyle={
+  color:"blueviolet",
+  backgroundColor:"lightgray",
+}
+function SubHeader() {
+  return <p style={subHeaderStyle}>This will be an exciting course</p>
+}
+
+function Header(){
+  return(
+    <div>
+      <MainHeader></MainHeader>
+      <SubHeader></SubHeader>
+    </div>
+  )
+}
+
+function Footer(){
+  return <p style={{color:"gray",backgroundColor:"black"}}>Happy Coding</p>;
+}
+
+function Students() {
+  const fullName = "Eduardo Guerra"
+  const programingExp = 2;
+
+  return (
+    <div className='container p-4'>
+        <div className='row'>Students Enrolled</div>
+        <div className='row border'>
+          <div className='col-1'>
+            <img src={`https://ui-avatars.com/api/?name=${fullName}`}
+            className='w-100' alt='asd'>
+            </img>
+          </div>
+          <div className='col-10'>
+            {fullName}
+            <br/>
+            Coding Experience {programingExp}
+
+          </div>
+        </div>
+
+    </div>
+  )
+}
+
+function MainBody(){
+  return (
+    <div>
+      <p>in this course we will learn about taskopedia</p>
+      <ul>
+        <li>call ben</li>
+        <li>goto Walmart</li>
+      </ul>
+      <div>
+        Enter task: <input maxLength={5} readOnly={false} placeholder='Guerra'></input>
+      </div>
+    </div>
+  )
+}
+
+root.render(
+  <>
+    <Header/>
+    <MainBody/>
+    <Footer/>
+    <Students/>
+  </>
+)
